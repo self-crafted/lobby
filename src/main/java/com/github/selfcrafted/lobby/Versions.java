ef7b2f2846bf9bf46814cd3a48bbb6beadff2665
offbyone <offbyone@disroot.org>
2022-02-24 21:49:43 +0100

    Fix more blossom replacements

--- src/main/java/com/github/selfcrafted/lobby/Versions.java
+++ src/main/java/com/github/selfcrafted/lobby/Versions.java
@@ -3,12 +3,12 @@ package com.github.selfcrafted.lobby;
 import java.util.List;
 
 public class Versions {
-    public static final String VERSION = "$version";
-    public static final String MINESTOM_VERSION = "$minestomVersion";
+    public static final String VERSION = "&version";
+    public static final String MINESTOM_VERSION = "&minestomVersion";
 
     public static List<String> getVersionLines() {
         return List.of(
-                "$Name: "+ Versions.VERSION,
+                "&Name: "+ Versions.VERSION,
                 "Minestom: "+Versions.MINESTOM_VERSION
         );
     }
